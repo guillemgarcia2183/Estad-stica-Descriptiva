4f8bd22b5f3c7b6d0a64794df98c8eab557c45fb
Pumpunkio <102738127+Pumpunkio@users.noreply.github.com>
2022-10-12 13:43:48 +0200

    Update E1_GrupG.R

--- E1_GrupG.R
+++ E1_GrupG.R
@@ -1,16 +1,16 @@
 "EXERCICI 1, APARTAT A"
 valoracions <- c(1,2,3,4,5,6,7,8,9,10) #VALORS DE LES VALORACIONS
 frequencies <- c(2,8,5,13,20,37,50,68,42,19) #FREQÜÈNCIA DE LES VALORACIONS EN ORDRE
-vectorvalors <- rep(valoracions, frequencies)
-table(vectorvalors) #TAULA DE FREQÜÈNCIES ABSOLUTES
-table(vectorvalors)/length(vectorvalors) #TAULA DE FREQÜÈNCIA RELATIVA
-cumsum(table(vectorvalors)) #TAULA DE FREQÜÈNCIES ABSOLUTES ACUMULADA
-cumsum(table(vectorvalors)/length(vectorvalors))#TAULA DE FREQÜÈNCIES RELATIVES ACUMULADA
+bellavista <- rep(valoracions, frequencies)
+table(bellavista) #TAULA DE FREQÜÈNCIES ABSOLUTES
+table(bellavista)/length(bellavista) #TAULA DE FREQÜÈNCIA RELATIVA
+cumsum(table(bellavista)) #TAULA DE FREQÜÈNCIES ABSOLUTES ACUMULADA
+cumsum(table(bellavista)/length(bellavista))#TAULA DE FREQÜÈNCIES RELATIVES ACUMULADA
 
 "APARTAT B"
-mean(vectorvalors) #MITJANA DE LA TAULA 
-median(vectorvalors) #MEDIANA DE LA TAULA
-var(vectorvalors) #VARIÀNCIA CORREGIDA
+mean(bellavista) #MITJANA DE LA TAULA 
+median(bellavista) #MEDIANA DE LA TAULA
+var(bellavista) #VARIÀNCIA CORREGIDA
 
 variancia <- function(x){
   mitjana <- mean(x)
@@ -20,9 +20,9 @@ variancia <- function(x){
   
   variacio
 }
-variancia(vectorvalors) #VARIÀNCIA
+variancia(bellavista) #VARIÀNCIA
 
-sd(vectorvalors) #DESVIACIÓ TÍPICA CORREGIDA
+sd(bellavista) #DESVIACIÓ TÍPICA CORREGIDA
 
 desv<-function(x){
   mitjana<-mean(x)
@@ -31,6 +31,28 @@ desv<-function(x){
   sum(fdd)
   res<-sqrt(fdd)
 }
-desv(vectorvalors) #DESVIACIÓ TÍPICA
+desv(bellavista) #DESVIACIÓ TÍPICA
 
 "APARTAT C"
+fitxer<-read.csv("R/bonambient.csv")
+bonambient<-fitxer$valoracions
+table(bonambient) #TAULA DE FREQÜÈNCIES ABSOLUTES
+table(bonambient)/length(bonambient) #TAULA DE FREQÜÈNCIA RELATIVA
+cumsum(table(bonambient)) #TAULA DE FREQÜÈNCIES ABSOLUTES ACUMULADA
+cumsum(table(bonambient)/length(bonambient))#TAULA DE FREQÜÈNCIES RELATIVES ACUMULADA
+
+mean(bonambient) #MITJANA 
+median(bonambient) #MEDIANA
+sd(bonambient) #DESVIACIÓ TÍPICA
+var(bonambient) #VARIÀNCIA 
+
+
+"APARTAT D"
+# DIAGRAMES DE BARRES
+x1<-table(bellavista)
+x2<-table(bonambient)
+barplot(x1, main="Hotel Bellavista", sub="Valoracions dels usuaris")
+barplot(x2, main="Hotel Bonambient", sub="Valoracions dels usuaris")
+# DIAGRAMES DE CAIXES
+boxplot(bellavista, main="Hotel Bellavista")
+boxplot(bonambient, main="Hotel Bonambient")
