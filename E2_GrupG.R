affc5b4f2a6173db7066e60c895983acbb8156a0
Guillem Garcia Dausà <102737703+guillemgarcia2183@users.noreply.github.com>
2022-11-19 12:57:04 +0100

    Add files via upload

--- E2_GrupG.R
+++ E2_GrupG.R
@@ -39,3 +39,77 @@ lengths(filter(fitxer, PES < 70 & EDAT <= 50))# 65 PERSONES QUE PESEN MENYS DE 7
 
 
 
+"EXERCICI 3"
+"APARTAT A"
+#REPRESENTACIÓ FUNCIÓ DE DENSITAT
+x <- seq(1,3, by = 0.005) 
+y <- 1/4 * ((x -1)^3)
+plot(x,y, type = "l", main = "FUNCIÓ DE DENSITAT", ylab = "f(x)")
+
+#CÀLCUL DE LA FUNCIÓ DE DISTRIBUCIÓ
+xdis <- seq(1,3, by = 0.005)
+ydis <- 1/16 * ((x-1)^4)
+plot(xdis,ydis, type = "l", main = "FUNCIÓ DE DISTRIBUCIÓ", xlab = "x", ylab = "F(x)")
+
+"APARTAT B"
+u <- runif(80000) #SIMULACIÓ AMB N = 80.000
+y <- (16*u)^(1/4) + 1 #INVERSA DE LA FUNCIÓ DE DISTRIBUCIÓ
+#HISTOGRAMA
+hist(y, freq = FALSE, main = "HISTOGRAMA EN N = 80.000", xlab= "F_inversa(u)", ylab = "Densitat")
+#DIBUIXEM TAMBÉ LA FUNCIÓ DE DENSITAT SOBRE L'HISTOGRAMA
+z <- seq(1,3, by = 0.005) 
+t <- 1/4 * ((z-1)^3)
+lines(z,t)
+
+"APARTAT C"
+mitjana_empirica <- mean(y) #MITJANA
+
+varp <- function(x){
+  mitjana <- mean(x)
+  n <- length(x)
+  sumatori <- sum((x - mitjana)**2)
+  variacio <- 1/n * sumatori
+  
+  variacio
+}
+varp_empirica <- varp(y) #VARIÀNCIA (NO CORREGIDA)
+  
+mitjana_empirica
+varp_empirica
+
+
+"APARTAT D"
+#REPRESENTEM LA DISTRIBUCIÓ NORMAL (0,1) EN UNA MOSTRA DE N = 80.000
+dnormal <- rnorm(80000,0,1)
+hist(dnormal, freq=FALSE, main="Histograma de la distribució N(0,1)")
+#COMPROVACIONS
+mean(dnormal)
+var(dnormal)
+
+#APLIQUEM EL TEOREMA CENTRAL DEL LÍMIT EN LA MOSTRA DE L'APARTAT B
+n <- 200
+mu <- mitjana_empirica
+sigma <- sqrt(varp_empirica)
+matriu <- matrix(y, nrow=400, ncol = 200)
+vector_mitjanes <- apply(matriu,1,mean)
+
+resultat <- (vector_mitjanes - mu)/(sigma/sqrt(n))
+hist(resultat, freq=FALSE, main="Histograma aplicant teorema central del límit", ylab="Densitat")
+#COMPROVACIONS
+mean(resultat)
+varp(resultat)
+
+"APARTAT E"
+#VEIEM QUIN ÉS EL VALOR T A PARTIR DE LA FUNCIÓ QUANTILA 
+valor <- qnorm(0.8, mitjana_empirica, varp_empirica)
+valor
+
+#VEIEM QUIN ÉS EL TOTAL DE MOSTRES QUE SON MENOR QUE EL VALOR
+sumatori <- 0
+for(i in seq_along(y)){
+  if (y[[i]] < valor){
+    sumatori <- sumatori + 1}
+}
+sumatori
+
+
